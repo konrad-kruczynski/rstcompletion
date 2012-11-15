b44d9d8b3c3f35d849994ed24f7487c40e53ecb3
Konrad Kruczyński <konrad.kruczynski@gmail.com>
2012-11-16 00:18:11 +0100

    Initial commit with the completion addin stub.

---
+++ RstCompletion/AssemblyInfo.cs
@@ -0,0 +1,10 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+
+[assembly: AssemblyTitle("RstCompletion")]
+[assembly: AssemblyDescription("Rst completion addin for MonoDevelop")]
+[assembly: AssemblyCompany("Konrad Kruczyński")]
+[assembly: AssemblyCopyright("Copyright 2012 Konrad Kruczyński")]
+
+[assembly: AssemblyVersion("0.1")]
+
